4ce7bd455e5aeaf9ca4926c5bf65748743e5a44d
RuthStiebel <141753629+RuthStiebel@users.noreply.github.com>
2025-01-17 16:29:09 +0200

    added UserLimitSchema:   palette-backend/src/models/paletteModels.ts

--- palette-backend/src/models/paletteModels.ts
+++ palette-backend/src/models/paletteModels.ts
@@ -1,22 +1,30 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, Model, Document } from "mongoose";
 export interface Color {
   hex: string;
   rgb: [number, number, number];
 }
-export interface Palette {
+export interface Palette extends Document {
   paletteId: string;
-  userId: string; 
+  userId: string;
   colors: Color[];
   createdAt: Date;
   shades: Color[][];
 }
 
-const ColorSchema = new Schema<Color>({
+export interface UserLimit extends Document {
+  userId: string;
+  count: number;
+  lastReset: Date;
+}
+
+// Define Color Schema
+const ColorSchema: Schema<Color> = new Schema({
   hex: { type: String, required: true }, 
   rgb: { type: [Number], required: true },
 });
 
-const PaletteSchema = new Schema<Palette>({
+// Define Palette Schema
+const PaletteSchema: Schema<Palette> = new Schema({
   paletteId: { type: String, required: true, unique: true },
   userId: { type: String, required: true },
   colors: [ColorSchema],
@@ -24,9 +32,19 @@ const PaletteSchema = new Schema<Palette>({
   shades: [ColorSchema],
 });
 
+const UserLimitSchema: Schema<UserLimit> = new Schema({
+  userId: { type: String, required: true, unique: true },
+  count: { type: Number, required: true, default: 0 },
+  lastReset: { type: Date, required: true, default: Date.now },
+});
+
+
 /*const userSchema = new Schema<User>({
   userId: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
 });*/
-export default mongoose.model<Palette>("Palette", PaletteSchema);
+
+// Export Models
+export const PaletteModel: Model<Palette> = mongoose.model<Palette>("Palette", PaletteSchema);
+export const UserLimitModel: Model<UserLimit> = mongoose.model<UserLimit>("UserLimit", UserLimitSchema);
