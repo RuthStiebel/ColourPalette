d1eff4c236aee0c5f58727bf86a0ed4ba7fbf520
Ruth Stiebel <stiebelruth@gmail.com>
2025-02-04 13:58:56 +0200

    Moved backend URL to globals 	modified:   palette-frontend/src/App.tsx 	modified:   palette-frontend/src/utils/globals.ts

--- palette-frontend/src/App.tsx
+++ palette-frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { Palette } from "../../palette-backend/src/models/paletteModels";
 import UserHistory from "./components/UserHistory";
 import PaletteGenerator from './components/PaletteGenerator'; 
 import { Container, Stack } from "@mui/material";
-import { MAX_NUM_COLORS } from "./utils/globals";
+import { MAX_NUM_COLORS, BACKEND_URL } from "./utils/globals";
 
 const App: React.FC = () => {
   const [keywords, setKeywords] = useState<string>("");
@@ -16,8 +16,7 @@ const App: React.FC = () => {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [popupMessage, setPopupMessage] = useState<string | null>(null);
-  const BACKEND_URL = "http://localhost:5000"; // Backend URL
-  // const BACKEND_URL = "https://colourpalettebackend.onrender.com";
+
 
   useEffect(() => {
     // Check if a userId is already in local storage
--- palette-frontend/src/utils/globals.ts
+++ palette-frontend/src/utils/globals.ts
@@ -1,1 +1,3 @@
 export const MAX_NUM_COLORS = 5; // Maximum number of colors
+export const BACKEND_URL = "http://localhost:5000"; // Backend URL
+// const BACKEND_URL = "https://colourpalettebackend.onrender.com";
