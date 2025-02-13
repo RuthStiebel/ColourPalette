5c050a454929cf7e9436e2916c8a544d5f950779
Ruth Stiebel <stiebelruth@gmail.com>
2025-02-13 23:44:41 +0200

    changed backend URL:   palette-frontend/src/utils/globals.ts

--- palette-frontend/src/utils/globals.ts
+++ palette-frontend/src/utils/globals.ts
@@ -1,3 +1,3 @@
 export const MAX_NUM_COLORS = 5; // Maximum number of colors
-//export const BACKEND_URL = "http://localhost:5000"; // Backend URL
-export const BACKEND_URL = "https://colourpalettebackend.onrender.com";
+export const BACKEND_URL = "http://localhost:5000"; // Backend URL
+//export const BACKEND_URL = "https://colourpalettebackend.onrender.com";
